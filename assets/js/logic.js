1652d98fd33705b8ff106aeaf08c2233b83dcbd9
Lianna T-F <apple@MacBook-Pro.lan>
2024-01-14 19:02:36 +0000

    created code for save button and saving data to local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,27 @@
 // display current date and time
 
-// $("#currentDay").text(moment().format('dddd, Do MMMM YYYY'));
+$("#currentDay").text(moment().format('dddd, Do MMMM YYYY'));
 
-const date = dayjs().format("dddd, D MMM YYYY");
-$("#currentDay").text(date);
+// $("#currentTime").text(moment().format('h:mm A'));
 
-const time = dayjs().format("HH:mm");
-$("#currentTime").text(time);
+// const date = dayjs().format("dddd, D MMM YYYY");
+// $("#currentDay").text(date);
+
+// const time = dayjs().format("HH:mm");
+// $("#currentTime").text(time);
 
 
+//Variable for save button
+const saveBtn = $(".saveBtn");
+
+// save text to local stroage
+saveBtn.on("click", function(){
+    var hourKey = $(this).siblings(".plan").attr("id")
+    var activity = $(this).siblings(".plan").val()
+    localStorage.setItem(hourKey, activity)
+})
+
+$(".plan").each(function(){
+    var hourKey = $(this).attr("id")
+    $(this).val(localStorage.getItem(hourKey))
+})

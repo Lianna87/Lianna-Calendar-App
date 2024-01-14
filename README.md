222b2115d899464246502610ce03a72ad8934bb2
Lianna T-F <apple@MacBook-Pro.lan>
2024-01-14 21:57:15 +0000

    set up setInterval to update clock every second

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# Lianna-Calendar-App
-
-
-# Third-Party APIs: Work Day Scheduler
+# Lianna-Calendar-App: Work Day Scheduler
 
 ## Your Task
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+The task was to create a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Day.js](https://day.js.org/docs/en/display/format) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,20 @@
-// display current date and time
-
+// display current date using moment.js
 $("#currentDay").text(moment().format('dddd, Do MMMM YYYY'));
 
-// $("#currentTime").text(moment().format('h:mm A'));
+// display current date using moment.js
+    // $("#currentTime").text(moment().format('HH:mm:ss'));
 
-// const date = dayjs().format("dddd, D MMM YYYY");
-// $("#currentDay").text(date);
+// function to display current time using moment.js
+function updateTime() {
+    const currentTime = moment().format('HH:mm:ss');
+    $("#currentTime").text(currentTime);
+}
 
-// const time = dayjs().format("HH:mm");
-// $("#currentTime").text(time);
+// call function to display current time
+updateTime()
+
+// setInterval to update current time every second
+setInterval(updateTime, 1000)
 
 
 //Variable for save button
@@ -16,14 +22,14 @@ const saveBtn = $(".saveBtn");
 
 // save text to local stroage
 saveBtn.on("click", function(){
-    var hourKey = $(this).siblings(".plan").attr("id")
-    var activity = $(this).siblings(".plan").val()
-    localStorage.setItem(hourKey, activity)
+    var hourKey = $(this).siblings(".plan").attr("id");
+    var activity = $(this).siblings(".plan").val();
+    localStorage.setItem(hourKey, activity);
 })
 
 $(".plan").each(function(){
-    var hourKey = $(this).attr("id")
-    $(this).val(localStorage.getItem(hourKey))
+    var hourKey = $(this).attr("id");
+    $(this).val(localStorage.getItem(hourKey));
 })
 
 //Variable for delete button
@@ -31,9 +37,9 @@ const deleteBtn = $(".deleteBtn");
 
 // remove text from local storgae
 deleteBtn.on("click", function() {
-    var hourKey = $(this).siblings(".plan").attr("id")
-    localStorage.removeItem(hourKey)
-    $(this).siblings(".plan").val("")
+    var hourKey = $(this).siblings(".plan").attr("id");
+    localStorage.removeItem(hourKey);
+    $(this).siblings(".plan").val("");
 })
 
 // colour code time block to reflect past, present or future
@@ -43,17 +49,17 @@ function timeBlockColor() {
     $(".time-block").each(function(){
     var rowHour = parseInt($(this).attr("id"))
     if(rowHour < currentHour) {
-        $(this).addClass("past")
+        $(this).addClass("past");
     }
     else if(rowHour === currentHour) {
-        $(this).addClass("present")
+        $(this).addClass("present");
     }
     else {
-        $(this).addClass("future")
+        $(this).addClass("future");
     }
     })
 }
-timeBlockColor()
+timeBlockColor();
 
 
 
